f55932e9785c264f39ae4c8425c1aee4922fa8f1
leticiamrosa <leticiamonteiro.dev@gmail.com>
2021-01-19 09:29:04 -0300

    refactor: group imports in index files

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -1,12 +1,11 @@
 import faker from 'faker'
 import { RemoteAuthentication } from '@data/usecases/authentication/remote-authentication'
-import { HttpPostClientSpy } from '@data/test/mock-http-client'
-import { mockAuthentication, mockAccountModel } from '@domain/test/mock-account'
-import { InvalidCredentialsError } from '@domain/errors/invalid-credentials-error'
+import { HttpPostClientSpy } from '@data/test'
+import { mockAuthentication, mockAccountModel } from '@domain/test'
+import { InvalidCredentialsError, UnexpectedError } from '@domain/errors'
 import { HttpStatusCode } from '@data/protocols/http/http-response'
-import { UnexpectedError } from '@domain/errors/unexpected-error'
-import { AuthenticationParams } from '@domain/usecases/authentication'
-import { AccountModel } from '@domain/models/account-model'
+import { AuthenticationParams } from '@domain/usecases'
+import { AccountModel } from '@domain/models'
 
 type SystemUnderTestTypes = {
   systemUnderTest: RemoteAuthentication
