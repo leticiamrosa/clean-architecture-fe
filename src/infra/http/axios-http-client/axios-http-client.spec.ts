eb27e66d513b394d32a4a01d47ec71161957402d
leticiamrosa <leticiamonteiro.dev@gmail.com>
2021-01-19 10:08:26 -0300

    test: ensure AxiosHttpClient calls axios with correct URL

---
+++ src/infra/http/axios-http-client/axios-http-client.spec.ts
@@ -0,0 +1,20 @@
+import { AxiosHttpClient } from './axios-http-client'
+import axios from 'axios'
+import faker from 'faker'
+
+jest.mock('axios')
+const mockedAxios = axios as jest.Mocked<typeof axios>
+
+describe('AxiosHttpClient', () => {
+  test('should call axios with correct URL', async () => {
+    // given
+    const url = faker.internet.url()
+    const sut = new AxiosHttpClient()
+
+    // when
+    await sut.post({ url })
+
+    // then
+    expect(mockedAxios).toHaveBeenCalledWith(url)
+  })
+})
