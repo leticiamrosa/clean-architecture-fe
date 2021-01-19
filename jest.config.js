be59ef4fc58f60fcdf4135d7aea7d41f9336f97a
leticiamrosa <leticiamonteiro.dev@gmail.com>
2021-01-19 12:16:52 -0300

    chore: add react configs

--- jest.config.js
+++ jest.config.js
@@ -12,8 +12,8 @@ module.exports = {
     '.+\\.(ts|tsx)$': 'ts-jest'
   },
   moduleNameMapper: {
-    "@domain/(.*)": "<rootDir>/src/domain/$1",
-    "@data/(.*)": "<rootDir>/src/data/$1",
-    "@infra/(.*)": "<rootDir>/src/infra/$1",
+    '@domain/(.*)': '<rootDir>/src/domain/$1',
+    '@data/(.*)': '<rootDir>/src/data/$1',
+    '@infra/(.*)': '<rootDir>/src/infra/$1'
   }
 }
