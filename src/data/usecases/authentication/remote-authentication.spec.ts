05494dff0f209eef61b14e8698e7c6d117efee03
leticiamrosa <leticiamonteiro.dev@gmail.com>
2021-01-19 09:01:31 -0300

    refactor: add generic params to HttpPostClient

--- src/data/usecases/authentication/remote-authentication.spec.ts
+++ src/data/usecases/authentication/remote-authentication.spec.ts
@@ -5,14 +5,16 @@ import { mockAuthentication } from '@domain/test/mock-authentication'
 import { InvalidCredentialsError } from '@domain/errors/invalid-credentials-error'
 import { HttpStatusCode } from '@data/protocols/http/http-response'
 import { UnexpectedError } from '@domain/errors/unexpected-error'
+import { AuthenticationParams } from '@domain/usecases/authentication'
+import { AccountModel } from '@domain/models/account-model'
 
 type SystemUnderTestTypes = {
   systemUnderTest: RemoteAuthentication
-  httpPostClientSpy: HttpPostClientSpy
+  httpPostClientSpy: HttpPostClientSpy<AuthenticationParams, AccountModel>
 };
 
 const makeSystemUnderTest = (url: string = faker.internet.url()): SystemUnderTestTypes => {
-  const httpPostClientSpy = new HttpPostClientSpy()
+  const httpPostClientSpy = new HttpPostClientSpy<AuthenticationParams, AccountModel>()
   const systemUnderTest = new RemoteAuthentication(url, httpPostClientSpy)
 
   return {
@@ -22,83 +24,87 @@ const makeSystemUnderTest = (url: string = faker.internet.url()): SystemUnderTes
 }
 
 describe('RemoteAuthentication', () => {
-  test('should call HttpPostClient with correct URL', async () => {
-    // given
-    const url = faker.internet.url()
-    const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest(url)
+  describe('HttpPostClient', () => {
+    test('should call HttpPostClient with correct URL', async () => {
+      // given
+      const url = faker.internet.url()
+      const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest(url)
 
-    // when
-    await systemUnderTest.auth(mockAuthentication())
+      // when
+      await systemUnderTest.auth(mockAuthentication())
 
-    // then
-    expect(httpPostClientSpy.url).toBe(url)
+      // then
+      expect(httpPostClientSpy.url).toBe(url)
+    })
+
+    test('should call HttpPostClient with correct body', async () => {
+      // given
+      const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
+      const authenticationParamsMock = mockAuthentication()
+
+      // when
+      await systemUnderTest.auth(authenticationParamsMock)
+
+      // then
+      expect(httpPostClientSpy.body).toEqual(authenticationParamsMock)
+    })
   })
 
-  test('should call HttpPostClient with correct body', async () => {
-    // given
-    const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
-    const authenticationParamsMock = mockAuthentication()
+  describe('HttpPostClient returns error', () => {
+    test('should throw UnexpectedError if HttpPostClient returns 400', async () => {
+      // given
+      const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
+      httpPostClientSpy.response = {
+        statusCode: HttpStatusCode.badRequest
+      }
 
-    // when
-    await systemUnderTest.auth(authenticationParamsMock)
+      // when
+      const result = systemUnderTest.auth(mockAuthentication())
 
-    // then
-    expect(httpPostClientSpy.body).toEqual(authenticationParamsMock)
-  })
+      // then
+      await expect(result).rejects.toThrow(new UnexpectedError())
+    })
 
-  test('should throw UnexpectedError if HttpPostClient returns 400', async () => {
-    // given
-    const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
-    httpPostClientSpy.response = {
-      statusCode: HttpStatusCode.badRequest
-    }
+    test('should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
+      // given
+      const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
+      httpPostClientSpy.response = {
+        statusCode: HttpStatusCode.unathorized
+      }
 
-    // when
-    const result = systemUnderTest.auth(mockAuthentication())
+      // when
+      const result = systemUnderTest.auth(mockAuthentication())
 
-    // then
-    await expect(result).rejects.toThrow(new UnexpectedError())
-  })
+      // then
+      await expect(result).rejects.toThrow(new InvalidCredentialsError())
+    })
 
-  test('should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
-    // given
-    const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
-    httpPostClientSpy.response = {
-      statusCode: HttpStatusCode.unathorized
-    }
+    test('should throw UnexpectedError if HttpPostClient returns 404', async () => {
+      // given
+      const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
+      httpPostClientSpy.response = {
+        statusCode: HttpStatusCode.notFound
+      }
 
-    // when
-    const result = systemUnderTest.auth(mockAuthentication())
+      // when
+      const result = systemUnderTest.auth(mockAuthentication())
 
-    // then
-    await expect(result).rejects.toThrow(new InvalidCredentialsError())
-  })
+      // then
+      await expect(result).rejects.toThrow(new UnexpectedError())
+    })
 
-  test('should throw UnexpectedError if HttpPostClient returns 404', async () => {
-    // given
-    const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
-    httpPostClientSpy.response = {
-      statusCode: HttpStatusCode.notFound
-    }
+    test('should throw UnexpectedError if HttpPostClient returns 500', async () => {
+      // given
+      const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
+      httpPostClientSpy.response = {
+        statusCode: HttpStatusCode.serverError
+      }
 
-    // when
-    const result = systemUnderTest.auth(mockAuthentication())
+      // when
+      const result = systemUnderTest.auth(mockAuthentication())
 
-    // then
-    await expect(result).rejects.toThrow(new UnexpectedError())
-  })
-
-  test('should throw UnexpectedError if HttpPostClient returns 500', async () => {
-    // given
-    const { systemUnderTest, httpPostClientSpy } = makeSystemUnderTest()
-    httpPostClientSpy.response = {
-      statusCode: HttpStatusCode.serverError
-    }
-
-    // when
-    const result = systemUnderTest.auth(mockAuthentication())
-
-    // then
-    await expect(result).rejects.toThrow(new UnexpectedError())
+      // then
+      await expect(result).rejects.toThrow(new UnexpectedError())
+    })
   })
 })
