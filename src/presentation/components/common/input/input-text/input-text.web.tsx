2edcc2c29f7dc8ec78350aa89e27d098f8df2f7a
leticiamrosa <leticiamonteiro.dev@gmail.com>
2021-01-21 08:11:27 -0300

    feat: remove autocomplete from chrome

--- src/presentation/components/common/input/input-text/input-text.web.tsx
+++ src/presentation/components/common/input/input-text/input-text.web.tsx
@@ -4,9 +4,13 @@ import Styles from './input-text.styles.scss'
 type Props = React.DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 const InputText: React.FC<Props> = (props: Props) => {
+  const onFocus = (event: React.FocusEvent<HTMLInputElement>): void => {
+    event.target.readOnly = false
+  }
+
   return (
     <div className={Styles.inputWrapper}>
-      <input {...props} />
+      <input {...props} readOnly onFocus={onFocus}/>
       <span className={Styles.status}>🔴</span>
     </div>
   )
