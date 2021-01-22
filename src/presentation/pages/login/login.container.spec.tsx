1f8742b96ec92eaf2ff01d1d346136aa5427190b
leticiamrosa <leticiamonteiro.dev@gmail.com>
2021-01-22 10:37:37 -0300

    test: ensure Login start with inputs with required state

--- src/presentation/pages/login/login.container.spec.tsx
+++ src/presentation/pages/login/login.container.spec.tsx
@@ -6,12 +6,21 @@ describe('Login Container', () => {
   test('should start with initial state', () => {
     // given
     const { getByTestId } = render(<Login />)
+
     // when
     const errorWrapper = getByTestId('error-wrapper')
     const submitButton = getByTestId('button-submit') as HTMLButtonElement
+    const inputEmail = getByTestId('input-email')
+    const inputPassword = getByTestId('input-password')
+    const inputEmailStatus = getByTestId('input-email-status')
+    const inputPasswordStatus = getByTestId('input-password-status')
 
     // expect
     expect(errorWrapper.childElementCount).toBe(0)
     expect(submitButton.disabled).toBe(true)
+    expect(inputEmail.title).toBe('Campo obrigatório')
+    expect(inputEmailStatus.textContent).toBe('🔴')
+    expect(inputPassword.title).toBe('Campo obrigatório')
+    expect(inputPasswordStatus.textContent).toBe('🔴')
   })
 })
