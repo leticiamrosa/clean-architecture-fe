8ce85afa14d3619baef64337528bbcc6583ce788
leticiamrosa <leticiamonteiro.dev@gmail.com>
2021-02-02 17:52:47 -0300

    feat: ensure ValidationComposite returns error when any validation fails

---
+++ src/presentation/validation/validators/test/mock-field-validation.ts
@@ -0,0 +1,13 @@
+import { FieldValidation } from '@presentation/validation/protocols/field-validation'
+
+export class FieldValidationSpy implements FieldValidation {
+  error: Error = null
+  value: string;
+
+  constructor (readonly field: string) {}
+
+  validate (value: string): Error {
+    this.value = value
+    return this.error
+  }
+}
