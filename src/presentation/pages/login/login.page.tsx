6ed75632cdaa91a9b4275147bd41e1f37e6f097b
leticiamrosa <leticiamonteiro.design@gmail.com>
2021-04-23 13:44:25 -0300

    feat: change Login composition to add SaveAccessToken

--- src/presentation/pages/login/login.page.tsx
+++ src/presentation/pages/login/login.page.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-import { Authentication } from '@domain/usecases'
+import { Authentication, SaveAccessToken } from '@domain/usecases'
 import Validation from '@presentation/protocols/validation'
 import LoginContainer from './login.container'
 
 type Props = {
   validation?: Validation
   authentication?: Authentication
+  saveAccessToken: SaveAccessToken
 }
 
 const LoginPage: React.FC<Props> = (props: Props) => {
