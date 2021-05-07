756e71fd443ded07edf77abb1516a9c6c12b90b4
leticiamrosa <leticiamonteiro.design@gmail.com>
2021-05-06 23:42:27 -0300

    test: ensure RemoteAddAccount calls HttpPostClient with correct URL

---
+++ src/data/usecases/add-account/remote-add-account.spec.ts
@@ -0,0 +1,37 @@
+import { RemoteAddAccount } from './remote-add-acount'
+import { HttpPostClientSpy } from '@data/test'
+import { AddAccountParams } from '@domain/usecases'
+import { AccountModel } from '@domain/models'
+import { mockAddAccountParams } from '@domain/test'
+
+import faker from 'faker'
+
+// SutTpes means SystemUnderTestTypes
+type SutTypes = {
+  sut: RemoteAddAccount
+  httpPostClientSpy: HttpPostClientSpy<AddAccountParams, AccountModel>
+}
+
+const makeSut = (url: string = faker.internet.url()): SutTypes => {
+  const httpPostClientSpy = new HttpPostClientSpy<AddAccountParams, AccountModel>()
+  const sut = new RemoteAddAccount(url, httpPostClientSpy)
+
+  return {
+    sut,
+    httpPostClientSpy
+  }
+}
+
+describe('RemoteAddAccount', (): void => {
+  it('should call HttpPostClient with correct URL', async () => {
+    // given
+    const url = faker.internet.url()
+    const { sut, httpPostClientSpy } = makeSut(url)
+
+    // when
+    await sut.add(mockAddAccountParams())
+
+    // then
+    expect(httpPostClientSpy.url).toBe(url)
+  })
+})
